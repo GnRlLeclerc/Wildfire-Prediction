266fe4f6f64fe768f83498d31721087e8e54874e
Thibaut <thibaut2saivre@gmail.com>
2025-01-15 22:22:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Wildfire Prediction
+
+Link to the [Kaggle Challenge](https://www.kaggle.com/datasets/abdelghaniaaba/wildfire-prediction-dataset).
+Deadline: 21st of February 2025.
+
+Additional rule: we are not allowed to use the `dataset/train` dataset labels.
+
+## Quickstart
+
+Install the necessary python dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+Download the dataset using the following command:
+
+```bash
+kaggle datasets download -d abdelghaniaaba/wildfire-prediction-dataset
+```
+
+Then unpack it into a `dataset/` directory:
+
+```bash
+unzip -q wildfire-prediction-dataset.zip -d dataset
+```
---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "Wildfire Prediction Kaggle Challenge";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      # Supported systems
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShell = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+              cudaSupport = true;
+            };
+          };
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # For Numpy, Torch, etc.
+            stdenv.cc.cc
+            zlib
+
+            # Plotting with GTK backend
+            gtk3
+            gobject-introspection
+
+            # GTK SVG image support
+            librsvg
+          ];
+
+          packages = with pkgs; [
+            (python312.withPackages (
+              ps: with ps; [
+                # Deep learning libraries
+                torch
+                torchvision
+                pillow
+                tqdm
+                numpy
+
+                # Load the dataset
+                kaggle
+
+                # Plotting
+                matplotlib
+                # GTK backend for matplotlib
+                pygobject3
+              ]
+            ))
+          ];
+        }
+      );
+    };
+}
